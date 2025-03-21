cc4e5181538d218e7c44f60303dd8811dbb4b324
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-03-21 22:59:36 +0530

    basic arithmatic operation of 2 numbers using user input

--- Basic java/addUserInput.java
+++ Basic java/addUserInput.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class addUserInput {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.println("first Number: ");
+        System.out.print("first Number: ");
         int a=sc.nextInt();
-        System.out.println("Second Number: ");
+        System.out.print("Second Number: ");
         int b=sc.nextInt();
         System.out.println("sum is :"+(a+b));
+        System.out.println("Substraction is :"+(a-b));
+        System.out.println("Product is: "+(a*b));
+        System.out.println("Divide is: "+(a/b));
     }
 }
