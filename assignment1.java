a4ed7a7902522e21c71ec0baa08baf6e062c60e3
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-04-01 19:50:57 +0530

    usage of continue statement

--- assignment1.java
+++ assignment1.java
@@ -1,14 +1,33 @@
-public class assignment1 {
-    public static void main(String[] args) {
-        byte b=4;
-        char c='a';
-        short s=512;
-        int i=1000;
-        float f=3.14f;
-        double d=99.99;
-        int $=24; //not recommended
+import java.util.Scanner;
 
-        double result=(f*b)+(i%c)-(d*s);
-        System.out.println(result);
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //1
+
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int c=sc.nextInt();
+
+        // float avg=(a+b+c)/3;
+        // System.out.println("Aerage is : "+avg);
+
+        //2
+
+        // int l=sc.nextInt();
+        // System.out.println("Area Of Square: "+(l*l));
+
+        //3
+
+        System.out.println("Pencil: ");
+        float pencil=sc.nextFloat();
+        System.out.println("Pen");
+        float pen=sc.nextFloat();
+        System.out.println("Eraser");
+        float Eraser=sc.nextFloat();
+
+        float total=(((pencil+pen+Eraser)*18)/100);
+        System.out.println("Total with 18% GST is: "+total);
     }
 }
---
+++ continuestatement.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class continuestatement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        do{
+            System.out.println("Enter Number:(break enter 0) ");
+            n=sc.nextInt();
+            if(n==0){
+                break;
+            }
+            if(n%10==0){
+                continue;
+            }
+            System.out.println(n);
+        }while (true); 
+    }  
+}
+
