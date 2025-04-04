1079aae052cadf9bb509f4bc4a5caf38b1cd44d1
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-04-05 00:51:26 +0530

    Binaomial of a number using function updated

--- Binomial.java
+++ Binomial.java
@@ -1,23 +1,39 @@
 import java.util.Scanner;
 
 public class Binomial{
-    public static int Binomial(int n,int r){
+
+    public static int factorial(int n){
         int x=1;
-        int y=1;
         for(int i=1;i<=n;i++){
             x=x*i;
         }
-        for(int j=1;j<=r;j++){
-            y=y*j;
-        }
-        int z=1;
-        for(int k=1;k<=(n-r);k++){
-            z=z*k;
-        }
-
-        int bin=x/(y*z);
-        return bin;
+        return x;
     }
+
+    public static int Binomial(int n,int r){
+        int fact_n=factorial(n);
+        int fact_r=factorial(r);
+        int fact_nmr=factorial(n-r);
+
+        int bincoefficient=fact_n/(fact_r*fact_nmr);
+        return bincoefficient;
+    }
+    //     int x=1;
+    //     int y=1;
+    //     for(int i=1;i<=n;i++){
+    //         x=x*i;
+    //     }
+    //     for(int j=1;j<=r;j++){
+    //         y=y*j;
+    //     }
+    //     int z=1;
+    //     for(int k=1;k<=(n-r);k++){
+    //         z=z*k;
+    //     }
+
+    //     int bin=x/(y*z);
+    //     return bin;
+    // }
 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
