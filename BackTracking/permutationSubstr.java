909c20c90248485c3bb58962f0f45b78a3d47d2e
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-06-08 00:21:35 +0530

    using BackTracking in finding all permutation of a given String

---
+++ BackTracking/permutationSubstr.java
@@ -0,0 +1,22 @@
+package BackTracking;
+
+public class permutationSubstr {
+
+    public static void findpermsubset(String str,String ans){
+        //base case
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            String NewStr=str.substring(0, i)+str.substring(i+1);
+            findpermsubset(NewStr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findpermsubset(str, "");
+    }
+}
