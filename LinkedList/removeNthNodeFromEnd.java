a264f807be7edb6f5485b7c9b4e450f1cc0a5a0e
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-06-14 12:01:01 +0530

    Removing element from Nth Node from Ending in LL

---
+++ LinkedList/removeNthNodeFromEnd.java
@@ -0,0 +1,170 @@
+package LinkedList;
+
+public class removeNthNodeFromEnd {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head=null;
+    public static Node tail=null;
+    public static int size=0;
+
+    public void addFirst(int data){
+         Node newnNode=new Node(data);
+         size++;
+        if(head==null){
+            head=tail=newnNode;
+            return;
+        }
+
+        newnNode.next=head;
+        head=newnNode;
+    }
+
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void addMiddle(int data,int idx){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("Empty LL");
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        if(size==0){
+            System.out.println("Empty LL");
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
+    
+    public int removeMiddle(int idx){
+        if(size==0){
+            System.out.println("Empty LL");
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+
+        Node prev=head;
+        for(int i=0;i<idx-1;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=prev.next.next;
+        size--;
+        return val;
+    }
+
+    public void printLL(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void RemoveNthNodeFromEnd(int idx){
+        //calculate size
+        int sz=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            sz++;
+        }
+
+        if(sz==idx){
+            head=head.next;
+            return;
+        }
+
+        int i=1;
+        int idxToFind=sz-idx;
+        Node prev=head;
+        while(i<idxToFind){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+    }
+
+    public static void main(String[] args) {
+        removeNthNodeFromEnd ll=new removeNthNodeFromEnd();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addMiddle(3, 2);
+        ll.printLL();
+        ll.RemoveNthNodeFromEnd(3);
+        ll.printLL();
+    }
+}
