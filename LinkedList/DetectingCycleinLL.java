205ed2ec90577b5b5e39ee75539565dac879a74b
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-06-15 20:55:18 +0530

    check whether the LL have Cycle or not in it

---
+++ LinkedList/DetectingCycleinLL.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class DetectingCycleinLL {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head=null;
+    public static Node tail=null;
+
+    public static boolean isCyclic(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=new Node(5);
+        head.next.next.next.next=head;
+        System.out.println(isCyclic(head));
+    }
+}
